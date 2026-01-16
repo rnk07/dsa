4d2c041c5bf6c4aeb4a702472ab9f1dffceb4662
rnk07 <patelrnk07@gmail.com>
2026-01-16 17:57:55 -0500

    leet1

---
+++ prac1.js
@@ -0,0 +1,30 @@
+var isValid = function (s) {
+
+
+    let stack = []
+    let map = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    }
+
+
+    for (let char of s) {
+
+        if(char === '(' || char === '{' || char === '[' ){
+            stack.push(char)
+        }
+        else{
+            if(stack.pop() !== map[char]){
+
+            return false
+            }
+        }
+
+
+    }
+    return stack.length === 0;
+
+};
+
+console.log(isValid("({})"))
