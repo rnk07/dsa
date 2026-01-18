d2c4e348c863338353db67207216a899d730eac1
rnk07 <patelrnk07@gmail.com>
2026-01-18 13:02:34 -0500

    prac20

---
+++ prac20.js
@@ -0,0 +1,21 @@
+function uniqueIdentifier(nums) {
+    for (let i = 0; i < nums.length; i++) {
+        let count = 0;
+
+        for (let j = 0; j < nums.length; j++) {
+
+            if (nums[i] === nums[j]) {
+                count++
+            }
+
+        }
+        if(count ===1){
+            return nums[i]
+        }
+
+    }
+    return -1
+}
+
+let nums = [20,  5, 5, 20, 30]
+console.log(uniqueIdentifier(nums))
