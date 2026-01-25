2b7afe6a31ec1638bb2f2f120f0a9e070c3b9680
rnk07 <patelrnk07@gmail.com>
2026-01-25 13:08:04 -0500

    prac7

---
+++ 03/prac7.js
@@ -0,0 +1,63 @@
+function merge(arr,left,mid, right){
+
+    let tempArr = []
+    let l = left
+    let r = mid+1
+    
+    while(l <= mid && r  <= right){
+
+        if(arr[l] <= arr[r]){
+            tempArr.push(arr[l])
+            l++
+        }else{
+            tempArr.push(arr[r])
+            r++
+        }
+
+    }
+
+    while(l <= mid){
+        tempArr.push(arr[l])
+        l++
+    }
+
+    while(r <= right){
+        r++
+    }
+
+
+    for(let i =0;i <tempArr.length;i++){
+
+        arr[left+i] = tempArr[i]
+    }
+
+
+
+}
+
+
+
+
+
+
+
+
+
+function sort(arr,left=0, right =arr.length-1){
+
+    if(left >= right) return
+
+    let mid = Math.floor((left+right)/2)
+
+    sort(arr,left,mid)
+    sort(arr,mid+1,right)
+    merge(arr,left,mid,right)
+
+return arr
+
+}
+
+
+
+let arr = [4,8,1,4,0,3]
+console.log(sort(arr))
