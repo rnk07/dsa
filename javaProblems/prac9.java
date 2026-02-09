670cf057c1b4ab9d794871747ed16a82754c2688
rnk07 <patelrnk07@gmail.com>
2026-02-09 16:06:59 -0500

    prac9

---
+++ javaProblems/prac9.java
@@ -0,0 +1,18 @@
+package leetCode.javaProblems;
+
+public class prac9{
+
+    public static void main(String[] xyz){
+
+        for(int i =0;i<5;i++){
+            System.out.println();
+
+            for(int j=0;j<=i;j++){
+                System.out.print("*");
+            }
+        }
+
+
+    }
+
+}
