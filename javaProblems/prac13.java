6ccc250ae283f1ece78916243c3d36c4c6d6859d
rnk07 <patelrnk07@gmail.com>
2026-02-10 22:40:27 -0500

    prac14

---
+++ javaProblems/prac13.java
@@ -0,0 +1,25 @@
+package leetCode.javaProblems;
+
+public class prac13 {
+    
+    public static int largstInArray(int[] arr){
+
+        int ans =0;
+
+        for(int i =0;i <arr.length;i++){
+            if(arr[i] > ans){
+                ans = arr[i];
+            }
+        }
+
+        return ans;
+    }
+
+    public static void main (String[] abc){
+    
+        int[] arr = {1,5,3,2,55,9,4,555,6};
+        System.out.println(largstInArray(arr));
+    }
+
+
+}
